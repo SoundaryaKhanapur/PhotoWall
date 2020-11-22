673b5ef02f5a95d25a6d718c999915b6cdf47b60
Soundarya <soundarya.m.khanapur@gmail.com>
2020-11-22 17:23:18 +0530

    formatting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const tasks = ['work','exercise','sleep'];
+const tasks = ['work', 'exercise', 'sleep'];
 
-const element = <ol>
-    { tasks.map((task, index) => <li key = {index}>{task}</li>)}   
-</ol>
+const element =
+    <div>
+        <h1> Task List </h1>
+        <ol>
+            {tasks.map((task, index) => <li key={index}>{task}</li>)}
+        </ol>
+    </div>
+
 
 ReactDOM.render(element, document.getElementById('root'));
