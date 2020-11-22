95eabdc46eb5456d27fb4528121c08e1674405e5
Soundarya <soundarya.m.khanapur@gmail.com>
2020-11-22 17:02:00 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const tasks = ['work','exercise','sleep'];
+
+const element = <ol>
+    <li>
+        Hello
+    </li>
+</ol>
+
+ReactDOM.render(element, document.getElementById('root'));
