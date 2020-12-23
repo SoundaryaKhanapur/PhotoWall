1f2a23e206ed6264ecdf8fc7b83cf7168b138d91
Soundarya <soundarya.m.khanapur@gmail.com>
2020-12-23 14:32:31 +0530

    exact path

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Title from './Title'
 import PhotoWall from './PhotoWall'
 import AddPhoto from './AddPhoto'
+import { Route } from 'react-router-dom'
 
 class Main extends Component {
     constructor() {
@@ -30,7 +31,7 @@ class Main extends Component {
         }
         this.removePhoto = this.removePhoto.bind(this);
         this.navigate = this.navigate.bind(this);
-        
+
     }
 
     removePhoto(postRemoved) {
@@ -40,7 +41,7 @@ class Main extends Component {
         }))
     }
 
-    navigate(){
+    navigate() {
         this.setState({
             screen: 'addPhoto'
         })
@@ -60,22 +61,17 @@ class Main extends Component {
     render() {
 
         return <div>
-            {
-                this.state.screen === 'photos' && (
-                    <div>
-                        <Title title={'Photowall'} />
-                        <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto} onNavigate = {this.navigate}/>
-                    </div>
-                )
-            }
-            {
-                this.state.screen === 'addPhoto' &&(
-                    <div>
-                        <AddPhoto />
-                    </div>
-                )
+            <Route exact path="/" render={() => (
+                <div>
+                    <Title title={'Photowall'} />
+                    <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto} onNavigate={this.navigate} />
+                </div>
+            )} />
 
-            }
+
+            <Route path="/AddPhoto" component = {AddPhoto}/>
+           
+
         </div>
     }
 }
