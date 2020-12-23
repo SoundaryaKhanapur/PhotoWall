4bef01ec22c9c853c9807b981c6f74a6a11a6d38
Soundarya <soundarya.m.khanapur@gmail.com>
2020-12-23 14:23:38 +0530

    router

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,43 +1,84 @@
 import React, { Component } from 'react'
-import Title from './Title';
+import Title from './Title'
 import PhotoWall from './PhotoWall'
- 
-class Main extends Component{
-    constructor(){
+import AddPhoto from './AddPhoto'
+
+class Main extends Component {
+    constructor() {
         super()
-        this.state={
-            posts : [{
+        this.state = {
+            posts: [{
                 id: "0",
                 description: "beautiful",
                 imageLink: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/types-of-flowers-1579719085.jpg"
             },
-            
-            { id: "1",
-            description: "beauty",
-            imageLink: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/types-of-flowers-1579719085.jpg"},
-            
-            { id: "2",
-            description: "beautiful",
-            imageLink: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/types-of-flowers-1579719085.jpg"}
-            ]
-            
+
+            {
+                id: "1",
+                description: "beauty",
+                imageLink: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/types-of-flowers-1579719085.jpg"
+            },
+
+            {
+                id: "2",
+                description: "beautiful",
+                imageLink: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/types-of-flowers-1579719085.jpg"
+            }
+            ],
+            screen: 'photos'
+
         }
         this.removePhoto = this.removePhoto.bind(this);
+        this.navigate = this.navigate.bind(this);
+        
     }
 
-    removePhoto(postRemoved){
+    removePhoto(postRemoved) {
         console.log(postRemoved.description)
         this.setState((state) => ({
             posts: state.posts.filter(post => post !== postRemoved)
         }))
     }
 
-    render(){
+    navigate(){
+        this.setState({
+            screen: 'addPhoto'
+        })
+    }
+
+    componentDidMount() {
+
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+        console.log(prevState.posts)
+        console.log(this.state)
+    }
+
+    render() {
+
         return <div>
-            <Title title={'Photowall'}/>
-           <PhotoWall posts = {this.state.posts} onRemovePhoto={this.removePhoto}/>
+            {
+                this.state.screen === 'photos' && (
+                    <div>
+                        <Title title={'Photowall'} />
+                        <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto} onNavigate = {this.navigate}/>
+                    </div>
+                )
+            }
+            {
+                this.state.screen === 'addPhoto' &&(
+                    <div>
+                        <AddPhoto />
+                    </div>
+                )
+
+            }
         </div>
     }
 }
+
 
 export default Main
--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import Photo from './Photo'
 import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
 
 
 function PhotoWall(props) {
-    return <div className="photo-grid">
-    { props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto}/>)}
-</div>
+    return <div>
+        <Link className="addIcon" to="/AddPhoto"></Link>
+        {/* <button onClick={props.onNavigate} className="addIcon"> </button> */}
+
+        <div className="photo-grid">
+            {props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto} />)}
+        </div>
+    </div>
 }
 
 PhotoWall.propTypes = {
